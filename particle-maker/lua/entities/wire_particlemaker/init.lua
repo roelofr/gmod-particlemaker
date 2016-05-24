f057bc90d48f755b135df1c4d47a5c34e4bd8775
Roelof Roos <roelof@roelof.io>
2016-05-24 07:38:00 +0200

    WIP reintegrating wiremod properly

---
+++ particle-maker/lua/entities/wire_particlemaker/init.lua
@@ -0,0 +1,253 @@
+/**
+ * Copyright 2016 Roelof Roos (SirQuack)
+ * Part of Particle Maker Garry's Mod Tool
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+function ENT:UpdateInputs()
+	if not WireAddon or WireLib == nil then return end
+
+	local WMD = self.Entity.wireModData
+
+	if WMD == nil or not WMD.On then
+		self.Inputs = WireLib.CreateSpecialInputs(self.Entity, {}, {})
+		return
+	end
+
+	local wireInputs = {}
+	table.insert( wireInputs, { "Fire", "NORMAL" } )
+
+		-- Change Colors
+	if WMD.Colour then
+		table.insert( wireInputs, { "Color1", "COLOR" } )
+		table.insert( wireInputs, { "Color2", "COLOR" } )
+	end
+
+		-- Main properties change
+	if WMD.Basic then
+		table.insert( wireInputs, { "Velocity", "NORMAL" } )
+		table.insert( wireInputs, { "Delay", "NORMAL" } )
+		table.insert( wireInputs, { "Spread", "NORMAL" } )
+		table.insert( wireInputs, { "DieTime", "NORMAL" } )
+	end
+		-- Only when advanced
+	if WMD.Effects then
+		table.insert( wireInputs, { "StartAlpha", "NORMAL" } )
+		table.insert( wireInputs, { "EndAlpha", "NORMAL" } )
+		table.insert( wireInputs, { "StartSize", "NORMAL" } )
+		table.insert( wireInputs, { "EndSize", "NORMAL" } )
+		table.insert( wireInputs, { "StartLength", "NORMAL" } )
+		table.insert( wireInputs, { "EndLength", "NORMAL" } )
+	end
+
+	if WMD.Advanced then
+		table.insert( wireInputs, { "RollRand", "NORMAL" } )
+		table.insert( wireInputs, { "RollDelta", "NORMAL" } )
+		table.insert( wireInputs, { "AirResistance", "NORMAL" } )
+		table.insert( wireInputs, { "Bounce", "NORMAL" } )
+		table.insert( wireInputs, { "Gravity", "NORMAL" } )
+		table.insert( wireInputs, { "Collide", "NORMAL" } )
+		table.insert( wireInputs, { "Lighting", "NORMAL" } )
+		table.insert( wireInputs, { "Sliding", "NORMAL" } )
+		table.insert( wireInputs, { "Align", "NORMAL" } )
+		table.insert( wireInputs, { "Stick", "NORMAL" } )
+		table.insert( wireInputs, { "DoubleSided", "NORMAL" } )
+		table.insert( wireInputs, { "AngleVelX", "NORMAL" } )
+		table.insert( wireInputs, { "AngleVelY", "NORMAL" } )
+		table.insert( wireInputs, { "AngleVelZ", "NORMAL" } )
+		table.insert( wireInputs, { "StickLifeTime", "NORMAL" } )
+		table.insert( wireInputs, { "StickStartSize", "NORMAL" } )
+		table.insert( wireInputs, { "StickEndSize", "NORMAL" } )
+		table.insert( wireInputs, { "StickStartAlpha", "NORMAL" } )
+		table.insert( wireInputs, { "StickEndAlpha", "NORMAL" } )
+	end
+
+	local InA, InB = {}, {}
+
+	for _,v in pairs(wireInputs) do
+		table.insert( InA, v[1] )
+		table.insert( InB, v[2] )
+	end
+
+	self.Inputs = WireLib.CreateSpecialInputs(self.Entity, InA, InB)
+end
+
+function ENT:GetWiremodSettings( Data )
+	self.Entity.wireModData = { On = false, Colour = false, Basic = false, Effects = false, Advanced = false }
+
+	local opts = self:KeyToNameValue( Data )
+	self.Entity.wireModData.On = Either( opts['wire_enabled'] ~= nil and opts['wire_enabled'], true, false )
+	self.Entity.wireModData.Colour = Either( opts['wire_colour'] ~= nil and opts['wire_colour'], true, false )
+	self.Entity.wireModData.Basic = Either( opts['wire_basic'] ~= nil and opts['wire_basic'], true, false )
+	self.Entity.wireModData.Effects = Either( opts['wire_effects'] ~= nil and opts['wire_effects'], true, false )
+	self.Entity.wireModData.Advanced = Either( opts['wire_advanced'] ~= nil and opts['wire_advanced'], true, false )
+
+	self:UpdateInputs()
+end
+
+function ENT:Initialize()
+	self:SetModel( "models/items/combine_rifle_ammo01.mdl" )
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+
+	local phys = self:GetPhysicsObject()
+
+	self.PhysgunDisabled = false
+
+	self.Entity.wireModData = { On = false, Colour = false, Basic = false, Effects = false, Advanced = false }
+
+	self.Firing = false
+	self.FiringWire = false
+
+	self:SetOverlayText( "Particle Emitter" )
+
+	self:UpdateInputs()
+end
+
+function ENT:OnRemove()
+	if WireAddon then
+		Wire_Remove(self.Entity)
+	end
+end
+
+function ENT:TriggerInput(Name, Value)
+	if (Name == "Fire") then
+		local Ent = self:GetTable()
+
+		if self:GetToggleWiremod() then
+			if Value ~= 0 and not self.hasToggledWM then
+				self:SetOn( 'wire', not self.FiringWire )
+				self.hasToggledWM = true
+			elseif Value == 0 and self.hasToggledWM then
+				self.hasToggledWM = false
+			end
+		else
+			if Value ~= 0 then
+				self:SetOn( 'wire', true )
+			elseif Value == 0 then
+				self:SetOn( 'wire', false )
+			end
+		end
+	elseif Name == "Color1" then
+		self:TriggerInput( "ColorR1", Value[1] )
+		self:TriggerInput( "ColorG1", Value[2] )
+		self:TriggerInput( "ColorB1", Value[3] )
+	elseif Name == "Color2" then
+		self:TriggerInput( "ColorR2", Value[1] )
+		self:TriggerInput( "ColorG2", Value[2] )
+		self:TriggerInput( "ColorB2", Value[3] )
+	else
+		for _,v in pairs(ParticleOptions) do
+			if (Name == v.Name) then
+				if not SinglePlayer() then
+					-- ALWAYS Clamp stuff in multiplayer.. because people are idiots T_T
+					Value = math.Clamp(Value, v.Min, v.Max)
+				end
+
+				if (v.Type == "Bool") then
+					Value = util.tobool(Value)
+				end
+
+				self:SetData({{Name = v.Name, Type = v.Type, Value = Value}})
+			end
+		end
+	end
+end
+
+
+function ENT:SetToggle(b)
+	self.Toggle = b or false
+end
+
+function ENT:GetToggle()
+	return self.Toggle
+end
+function ENT:SetToggleWiremod(b)
+	self.ToggleWiremod = b or false
+end
+
+function ENT:GetToggleWiremod()
+	return self.ToggleWiremod
+end
+
+function ENT:SetOn(source, on)
+	on = util.tobool( on )
+	local onBefore = self:GetOn()
+	if source == "key" then
+		self.Firing = on
+	elseif source == "wire" then
+		self.FiringWire = on
+	end
+	local isOn = ( self.Firing or self.FiringWire )
+
+	self.Entity:SetNetworkedBool("Activated", isOn)
+
+	local shouldFireShot = ( isOn ~= onBefore and isOn )
+	if shouldFireShot then
+		self:FireShot()
+	end
+end
+
+function ENT:GetOn()
+	return self.Firing or self.FiringWire
+end
+
+function ENT:SetDelay(f)
+	self.Delay = f
+	self.Entity:SetNetworkedFloat("Delay", f)
+end
+
+function ENT:GetDelay()
+	return self.Delay
+end
+
+function ENT:FireShot()
+
+	local Pos = self.Entity:GetPos()
+	Pos = Pos + self.Entity:GetUp() * 4
+
+	// Make the effects
+	local Effect = EffectData()
+		Effect:SetOrigin(Pos)
+		Effect:SetEntity(self.Entity)
+	util.Effect("particle_custom", Effect)
+
+end
+
+
+/*---------------------------------------------------------
+   Name: OnTakeDamage
+---------------------------------------------------------*/
+function ENT:OnTakeDamage(Dmginfo)
+	self.Entity:TakePhysicsDamage(Dmginfo)
+end
+/*---------------------------------------------------------
+   Name: CanTool
+   Allow all tools
+---------------------------------------------------------*/
+function ENT:CanTool( ply, trace, mode )
+	if string.find( mode, "duplicator", nil, false ) != nil then
+		net.Start( "ParticleMakerDupe" )
+		net.Send( ply )
+		return false
+	end
+
+	return true
+
+end
--- particle-maker/lua/weapons/gmod_tool/stools/particle_maker.lua
+++ particle-maker/lua/weapons/gmod_tool/stools/particle_maker.lua
@@ -20,40 +20,17 @@ TOOL.Name			= "#tool.particle_maker.name"
 TOOL.Command		= nil
 TOOL.ConfigName		= ""
 
--- The 'default' convars, will always be there
+-- These convars are not part of any preset, so are defined here.
 TOOL.ClientConVar["Weld"]	= "1"
 TOOL.ClientConVar["Frozen"]	= "1"
 
-TOOL.ClientConVar["wire_enabled"]	= "1"
-TOOL.ClientConVar["wire_basic"]		= "1"
-TOOL.ClientConVar["wire_colour"]	= "0"
-TOOL.ClientConVar["wire_effects"]	= "0"
-TOOL.ClientConVar["wire_advanced"]	= "0"
-
 TOOL.ClientConVar["Key"]	= "5"
 TOOL.ClientConVar["Toggle"]	= "0"
-TOOL.ClientConVar["Toggle_Wire"] = "0"
 
 ParticleOptions = SirQuack.ParticleMaker.getParticleOptions()
 PresetOptions = SirQuack.ParticleMaker.getPresetOptions()
 
 local _HasWiremod = SirQuack.ParticleMaker.hasWiremod()
-
-local function CreateSegment(CPanel, title)
-    local Panel = vgui.Create("DForm", CPanel)
-
-    Panel:SetSizeX( false )
-	Panel:Dock( TOP )
-	Panel:DockPadding( 10, 10, 10, 0 );
-
-    Panel:SetLabel(title)
-
-    CPanel:AddPanel(Panel)
-
-    Panel:InvalidateLayout(true)
-
-    return Panel
-end
 
 -- Add all settings
 for _,v in pairs(ParticleOptions) do
@@ -113,13 +90,13 @@ function TOOL:GetValues()
 	return Data
 end
 
-local function SetValues(Ent, Data, Toggle, ToggleWiremod)
+local function SetValues(Ent, Data, Toggle)
 	if (type(Data) == "table") then
 		local PMTable = Ent:GetTable()
 
 		PMTable:SetToggle(Toggle)
-		PMTable:SetToggleWiremod(ToggleWiremod)
-		PMTable:SetData(Data)
+
+        SirQuack.ParticleMaker.entitySet(Ent, Data)
 
 		PMTable:UpdateInputs()
 	end
@@ -128,67 +105,75 @@ local function SetValues(Ent, Data, Toggle, ToggleWiremod)
 end
 -- duplicator.RegisterEntityModifier("particle_maker", SetValues)
 
-function TOOL:LeftClick( Trace )
-	if Trace.Entity and Trace.Entity:IsPlayer() then return false end
-	if SERVER and not util.IsValidPhysicsObject( Trace.Entity, Trace.PhysicsBone ) then return false end
+function TOOL:LeftClick( trace )
+	if trace.Entity and trace.Entity:IsPlayer() then return false end
+
+	if SERVER and !util.IsValidPhysicsObject( trace.Entity, trace.PhysicsBone )
+    then return false end
+
 	if CLIENT then return true end
 
-	local Ply = self:GetOwner()
-	local _3D = self:GetClientNumber("3D") == 1
-	local Toggle = self:GetClientNumber("Toggle") == 1
-	local ToggleWiremod = self:GetClientNumber("Toggle_Wire") == 1
-	local UseWiremod = self:GetClientNumber("wire_enable")
-	local Key = self:GetClientNumber("Key")
-	local Data = self:GetValues()
+	local ply = self:GetOwner()
+	local _3d = self:GetClientNumber("3D") == 1
+	local toggle = self:GetClientNumber("Toggle") == 1
+	local key = self:GetClientNumber("Key")
+	local data = self:GetValues()
 
-	local Material = self:GetClientInfo("Material")
+    PrintTable(data)
 
-	if Material == nil or Material == NULL or string.len( Material ) == 0 then
+	local material = self:GetClientInfo("Material")
+
+	if material == nil or material == NULL or string.len( material ) == 0 then
 		net.Start( "ParticleMakerError" )
 			net.WriteString( "#tool.particle_maker.error.no_material" )
-		net.Send( Ply )
+		net.Send( ply )
 		return false
 	end
 
-	local keyAsNum = tonumber( Key )
-	if ( Key == nil or Key == NULL or not Key or keyAsNum <= 0 ) and ( UseWiremod ~= 1 or not _HasWiremod ) then
+	local keyAsNum = tonumber( key )
+
+    if key == nil or key == NULL or keyAsNum <= 0 then
+        key = false
+    end
+
+    if not key and not _HasWiremod then
 		net.Start( "ParticleMakerError" )
 			net.WriteString( "#tool.particle_maker.error.input" )
-		net.Send( Ply )
+		net.Send( ply )
 		return false
 	end
 
 	-- We shot an existing particle maker - just change its values
-	if (
-        Trace.Entity:IsValid() and
-        (Trace.Entity:GetClass() == "gmod_particlemaker" or
-        Trace.Entity:GetClass() == "wire_particlemaker") and
-        Trace.Entity:GetPlayer() == Ply
-    ) then
-		SetValues(Trace.Entity, Data, Toggle, ToggleWiremod)
-		Trace.Entity:GetWiremodSettings( Data )
-		DoPropSpawnedEffect(Trace.Entity)
+	if
+        SirQuack.ParticleMaker.IsValid(trace.Entity) and
+        trace.Entity:GetPlayer() == ply
+    then
+		SetValues(trace.Entity, data, toggle)
+		//trace.Entity:GetWiremodSettings(data)
+		DoPropSpawnedEffect(trace.Entity)
 		return true
 	end
 
 	if not self:GetSWEP():CheckLimit("particle_makers") then return false end
 
-	local ParticleMaker = MakeParticle( Ply, Trace.HitPos, Data, Toggle, ToggleWiremod, Key, _3D )
-	local Angle = Trace.HitNormal:Angle()
-		Angle:RotateAroundAxis( Angle:Right(), -90 )
-	ParticleMaker:SetAngles( Angle )
+	local _entity = MakeParticle( ply, trace.HitPos, data, toggle, key, _3d )
 
-	local Weld
+	local angle = trace.HitNormal:Angle()
+	angle:RotateAroundAxis( angle:Right(), -90 )
+
+	_entity:SetAngles( angle )
+
+	local weld
 	if Trace.Entity:IsValid() then
 		if self:GetClientNumber("Weld") == 1 then
-			Weld = constraint.Weld(ParticleMaker, Trace.Entity, 0, Trace.PhysicsBone, 0, 0, true)
+			weld = constraint.Weld(_entity, trace.Entity, 0, trace.PhysicsBone, 0, 0, true)
 		end
 	end
 
 	undo.Create("particle_maker")
-		undo.AddEntity(ParticleMaker)
-		undo.AddEntity(Weld)
-		undo.SetPlayer(Ply)
+		undo.AddEntity(particlemaker)
+		undo.AddEntity(weld)
+		undo.SetPlayer(ply)
 	undo.Finish()
 
 	return true
@@ -257,439 +242,5 @@ if (SERVER) then
 end
 
 function TOOL.BuildCPanel(CPanel)
-
-	-- MAIN HEADER
-	CPanel:Help("#tool.particle_maker.desc")
-
-    SirQuack.ParticleMaker.runOnce(CPanel)
-
-    --[[
-        PRESETS
-    ]]
-
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.presets")
-
-    CPanel:AddPanel(frm)
-
-    local ctrl = vgui.Create("ControlPresets", frm)
-
-    ctrl:SetPreset( PresetOptions.folder )
-
-    for k, v in pairs( PresetOptions.options ) do ctrl:AddOption( k, v ) end
-    for k, v in pairs( PresetOptions.cvars ) do ctrl:AddConVar( k, v ) end
-
-    frm:AddItem(ctrl)
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-    --[[
-        GENERIC SETTINGS
-    ]]
-
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.generic")
-
-    CPanel:AddPanel(frm)
-
-    --  Weld to props?
-    frm:CheckBox("#tool.particle_maker.weld", "particle_maker_Weld")
-
-    -- Spawn frozen?
-    frm:CheckBox("#tool.particle_maker.frozen", "particle_maker_Frozen")
-
-    -- Toggle
-    frm:CheckBox("#tool.particle_maker.toggle", "particle_maker_Toggle")
-
-	-- Activation button
-    local ctrl = vgui.Create("CtrlNumPad", frm)
-    ctrl:SetConVar1("particle_maker_Key")
-    ctrl:SetLabel1("#tool.particle_maker.key")
-    frm:AddItem(ctrl)
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-    --[[
-        COLOUR SETTINGS
-    ]]
-
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.color")
-
-    CPanel:AddPanel(frm)
-
-    -- Color 1
-	local ctrl = vgui.Create("DColorMixer", frm)
-
-    ctrl:SetLabel("#tool.particle_maker.color.1")
-	ctrl:SetConVarR("particle_maker_ColorR1")
-    ctrl:SetConVarG("particle_maker_ColorG1")
-	ctrl:SetConVarB("particle_maker_ColorB1")
-
-    ctrl:SetAlphaBar(false)
-    ctrl:SetPalette(true)
-
-    frm:AddItem(ctrl, nil)
-
-	-- Color 2
-	local ctrl = vgui.Create("DColorMixer", frm)
-
-    ctrl:SetLabel("#tool.particle_maker.color.2")
-	ctrl:SetConVarR("particle_maker_ColorR2")
-    ctrl:SetConVarG("particle_maker_ColorG2")
-	ctrl:SetConVarB("particle_maker_ColorB2")
-
-    ctrl:SetAlphaBar(false)
-    ctrl:SetPalette(true)
-
-    frm:AddItem(ctrl, nil)
-
-    -- Random color
-    frm:CheckBox(
-        "#tool.particle_maker.color.mix",
-        "particle_maker_ColorRand"
-    )
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-    --[[
-        MATERIAL SETTINGS
-    ]]
-
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.material")
-    CPanel:AddPanel(frm)
-
-    local ctrl = vgui.Create( "MatSelect", frm )
-    ctrl:SetItemWidth( 64 )
-    ctrl:SetItemHeight( 64 )
-    ctrl:SetNumRows( 3 )
-    ctrl:SetConVar( "particle_maker_Material" )
-
-    Derma_Hook( ctrl.List, "Paint", "Paint", "Panel" )
-
-	for name, value in pairs(SirQuack.ParticleMaker.getParticles()) do
-        ctrl:AddMaterialEx( name, value, nil, {
-            particle_maker_Material = value
-        })
-	end
-
-    frm:AddItem(ctrl, nil)
-
-	-- Material textbox
-	frm:TextEntry("#tool.particle_maker.material", "particle_maker_Material")
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-    --[[
-        EFFECT SETTINGS
-    ]]
-
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.effect")
-    CPanel:AddPanel(frm)
-
-	-- Fire delay
-	frm:NumSlider(
-        "#tool.particle_maker.fire_delay",
-        "particle_maker_Delay",
-        0.001, 10, 2
-    )
-
-	-- Number particles
-	frm:NumSlider(
-        "#tool.particle_maker.partice_count",
-        "particle_maker_Number",
-		1, 10, 0
-	)
-
-	-- Velocity
-	frm:NumSlider(
-        "#tool.particle_maker.velocity",
-        "particle_maker_Velocity",
-		1, 10000, 0
-	)
-
-	-- Spread
-	frm:NumSlider(
-        "#tool.particle_maker.spread",
-        "particle_maker_Spread",
-		0, 360, 0
-	)
-
-	-- Die time
-	frm:NumSlider(
-        "#tool.particle_maker.die_time",
-        "particle_maker_DieTime",
-		1, 10, 1
-	)
-
-	-- Start alpha
-	frm:NumSlider(
-        "#tool.particle_maker.alpha.start",
-        "particle_maker_StartAlpha",
-		0, 255, 0
-	)
-
-	-- End alpha
-	frm:NumSlider(
-        "#tool.particle_maker.alpha.end",
-        "particle_maker_EndAlpha",
-		0, 255, 0
-	)
-
-	-- Start size
-	frm:NumSlider(
-        "#tool.particle_maker.size.start",
-        "particle_maker_StartSize",
-		0, 100, 0
-	)
-
-	-- End size
-	frm:NumSlider(
-        "#tool.particle_maker.size.end",
-        "particle_maker_EndSize",
-		0, 100, 0
-	)
-
-	-- Start length
-	frm:NumSlider(
-        "#tool.particle_maker.length.start",
-        "particle_maker_StartLength",
-		0, 100, 0
-	)
-
-	-- End length
-	frm:NumSlider(
-        "#tool.particle_maker.length.end",
-        "particle_maker_EndLength",
-		0, 100, 0
-	)
-
-	-- Roll
-	frm:NumSlider(
-        "#tool.particle_maker.roll_speed",
-        "particle_maker_RollRand",
-		0, 10, 2
-	)
-
-	-- Roll delta
-	frm:NumSlider(
-        "#tool.particle_maker.roll_delta",
-        "particle_maker_RollDelta",
-		-10, 10, 2
-	)
-
-	-- Air resistance
-	frm:NumSlider(
-        "#tool.particle_maker.air_resistance",
-        "particle_maker_AirResistance",
-		0, 1000, 0
-	)
-
-	-- Bounce
-	frm:NumSlider(
-        "#tool.particle_maker.bounce",
-        "particle_maker_Bounce",
-		0, 10, 2
-	)
-
-	-- Gravity
-	frm:NumSlider(
-        "#tool.particle_maker.gravity",
-        "particle_maker_Gravity",
-		-1000, 1000, 0
-	)
-
-	-- Collision
-	local _cld = frm:CheckBox("#tool.particle_maker.collide", "particle_maker_Collide")
-
-	-- Lighting
-	frm:CheckBox("#tool.particle_maker.lighting", "particle_maker_Lighting")
-
-	-- Slide
-	local _sld = frm:CheckBox("#tool.particle_maker.sliding", "particle_maker_Sliding")
-	frm:ControlHelp("#tool.particle_maker.sliding.help")
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-    --[[
-        3D SETTINGS
-    ]]
-    local frm = vgui.Create("DForm", CPanel)
-    frm:SetLabel("#tool.particle_maker.hdr.3d")
-    CPanel:AddPanel(frm)
-
-	-- Toggle 3D
-	local _3d = frm:CheckBox("#tool.particle_maker.3d", "particle_maker_3D")
-
-	-- Double sided
-	local _3dt = frm:CheckBox(
-        "#tool.particle_maker.doublesided", "particle_maker_DoubleSided")
-    frm:ControlHelp("#tool.particle_maker.doublesided.help")
-
-	-- Stick
-	local _3ds = frm:CheckBox(
-        "#tool.particle_maker.stick", "particle_maker_Stick")
-    frm:ControlHelp("#tool.particle_maker.stick.help")
-
-	-- Align
-	local _3da = frm:CheckBox(
-        "#tool.particle_maker.align", "particle_maker_Align")
-    frm:ControlHelp("#tool.particle_maker.align.help")
-
-	-- Angle velocity X
-	frm:NumSlider(
-		"#tool.particle_maker.angvel.x",
-        "particle_maker_AngleVelX",
-		-500, 500, 2
-	)
-
-	-- Angle velocity Y
-	frm:NumSlider(
-		"#tool.particle_maker.angvel.y",
-        "particle_maker_AngleVelY",
-		-500, 500, 2
-	)
-
-	-- Angle velocity Z
-	frm:NumSlider(
-		"#tool.particle_maker.angvel.z",
-        "particle_maker_AngleVelZ",
-		-500, 500, 2
-	)
-
-	-- Stick lifetime
-	frm:NumSlider(
-		"#tool.particle_maker.sticklifetime",
-        "particle_maker_StickLifeTime",
-		0.01, 10, 2
-	)
-
-	-- Stick start size
-	frm:NumSlider(
-        "#tool.particle_maker.stickstartsize", "particle_maker_StickStartSize",
-        0, 100, 2
-	)
-
-	-- Stick end size
-	frm:NumSlider(
-        "#tool.particle_maker.stickendsize", "particle_maker_StickEndSize",
-        0, 100, 2
-	)
-
-	-- Stick start alpha
-	frm:NumSlider(
-        "#tool.particle_maker.stickstartalpha",
-        "particle_maker_StickStartAlpha",
-        0, 255, 2
-	)
-
-	-- Stick end alpha
-	frm:NumSlider(
-        "#tool.particle_maker.stickendalpha", "particle_maker_StickEndAlpha",
-        0, 255, 2
-	)
-
-    frm:SizeToContentsY()
-    frm:InvalidateLayout()
-
-	-- Check if wire exists
-
-	-- WIRE HEADER
-	-- Header( CPanel, "Wiremod settings" )
-	-- CPanel:AddControl("Label", { Text = "If you want to control your emitter with Wiremod, check the box below. Most users will have sufficient control with Basic" })
-    --
-	-- -- Add enable wire checkbox
-	-- CPanel:AddControl("Checkbox", {
-    --    Label = "Enable wire inputs",
-    --    Command = "particle_maker_wire_enable"
-    -- })
-	-- CPanel:AddControl("Label", {
-    --    Text = "A \"Fire\" output is automatically added when the box above is checked.\nCheckboxes below will only have effect if you've enabled the checkbox above."
-    -- })
-    --
-	-- -- Add all checkboxes
-	-- CPanel:AddControl("Checkbox", {
-    --    Label = "Basic particle controls",
-    --    Command = "particle_maker_wire_basic"
-    -- })
-	-- CPanel:AddControl("Checkbox", {
-    --    Label = "Colour controls",
-    --    Command = "particle_maker_wire_colour"
-    -- })
-	-- CPanel:AddControl("Checkbox", {
-    --    Label = "Effect controls",
-    --    Command = "particle_maker_wire_effects"
-    -- })
-	-- CPanel:AddControl("Checkbox", {
-    --    Label = "Advanced particle controls",
-    --    Command = "particle_maker_wire_advanced"
-    -- })
-
-    --[[
-        Helper methods
-    ]]
-
-    -- Enforce valid rules
-    _cld.OnChange = function()
-        if not _cld:GetChecked() then
-            _sld:SetChecked( false )
-        end
-    end;
-
-    _sld.OnChange = function()
-        if _sld:GetChecked() then
-            _cld:SetChecked( true )
-            _3ds:SetChecked( false )
-            _3da:SetChecked( false )
-        end
-    end
-
-    _3d.OnChange = function()
-        if not _3d:GetChecked() then
-            _3dt:SetChecked( false )
-            _3ds:SetChecked( false )
-            _3da:SetChecked( false )
-        end
-    end
-
-    _3dt.OnChange = function()
-        if _3dt:GetChecked() then
-            _3d:SetChecked( true )
-        end
-    end
-
-    _3ds.OnChange = function()
-        if _3ds:GetChecked() then
-            _3d:SetChecked( true )
-        end
-    end
-
-    _3da.OnChange = function()
-        if _3da:GetChecked() then
-            _3d:SetChecked( true )
-            _3ds:SetChecked( true )
-        end
-    end
-
-    if not _3d:GetChecked() then
-        _3dt:SetChecked( false )
-        _3ds:SetChecked( false )
-        _3da:SetChecked( false )
-    end
-
-    if not _cld:GetChecked() then
-        _sld:SetChecked( false )
-    end
-
-    if _sld:GetChecked() then
-        _3ds:SetChecked( false )
-        _3da:SetChecked( false )
-    end
+    SirQuack.ParticleMaker.controlPanel(CPanel)
 end
